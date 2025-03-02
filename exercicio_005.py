5b5775287112a4ea761c67098210154fb7b49120
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-03-02 19:55:21 -0300

    Adicionando o exercício 005

---
+++ exercicio_005.py
@@ -0,0 +1,7 @@
+#Programa de algarismos vizinhos de um número
+
+numero = int(input("Digite um número: "))
+
+print("-" * 100)
+print(f"O antecessor do número {numero} é {numero - 1}.")
+print(f"O sucessor do número {numero} é {numero + 1}.")
