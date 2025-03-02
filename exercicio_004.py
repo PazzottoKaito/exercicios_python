b987660199d7dba22a82786e94b27fe717ad7781
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-03-02 19:48:45 -0300

    Adicionando o exercício 004

---
+++ exercicio_004.py
@@ -0,0 +1,17 @@
+#Programa para mostrar informações de uma variável
+
+texto = str(input("Digite algo: "))
+
+print("-" * 100)
+print(f"O tipo primitivo desse dado é: {type(texto)}.")
+print(f"O dado é um espaço vazio: {texto.isspace()}.")
+
+print("-" * 100)
+print(f"O dado é apenas alfabético: {texto.isalpha()}.")
+print(f"O dado é apenas numérico: {texto.isnumeric()}.")
+print(f"O dado é alfanumérico: {texto.isalnum()}.")
+
+print("-" * 100)
+print(f"O dado está em letras minúsculas: {texto.islower()}.")
+print(f"O dado está em letras maiúsculas: {texto.isupper()}.")
+print(f"O dado está capitalizado: {texto.istitle()}.")
