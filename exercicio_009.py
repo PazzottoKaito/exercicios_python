e79569c11fbfe32a01398fe0452f98e7b14b5298
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-03-02 20:25:38 -0300

    Adicionando o exercício 009

---
+++ exercicio_009.py
@@ -0,0 +1,16 @@
+#Programa para fazer tabuada de um número inteiro
+
+numero = int(input("Digite um número: "))
+
+print("-" * 100)
+print(f"A tabuada de multiplicação de 1 a 10 do número {numero}:"
+      f"\n{numero} X 01 = {numero * 1}."
+      f"\n{numero} X 02 = {numero * 2}."
+      f"\n{numero} X 03 = {numero * 3}."
+      f"\n{numero} X 04 = {numero * 4}."
+      f"\n{numero} X 05 = {numero * 5}."
+      f"\n{numero} X 06 = {numero * 6}."
+      f"\n{numero} X 07 = {numero * 7}."
+      f"\n{numero} X 08 = {numero * 8}."
+      f"\n{numero} X 09 = {numero * 9}."
+      f"\n{numero} X 10 = {numero * 10}.")
