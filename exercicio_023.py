d1792eebe7fd8139d3bba9a320b5202ca80c7171
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-03-03 20:14:38 -0300

    Adicionando o exercício 023

---
+++ exercicio_023.py
@@ -0,0 +1,10 @@
+#Programa para separar números inteiros
+
+numero = int(input("Digite um número com quatro algarismos: "))
+
+print("-" * 100)
+print(f"O número digitado, {numero}, é composto por:"
+      f"\n{numero // 1000 % 10} unidade(s) de milhar."
+      f"\n{numero // 100 % 10} centena(s)."
+      f"\n{numero // 10 % 10} dezena(s)."
+      f"\n{numero // 1 % 10} unidade(s).")
