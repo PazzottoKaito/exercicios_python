1b2a6209aa4d01ab134bf30ce70dc68c7ca7bafa
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-03-04 21:47:39 -0300

    Adicionando o exercício 027

---
+++ exercicio_027.py
@@ -0,0 +1,9 @@
+#Programa para achar o nome e o último sobrenome
+
+nome_completo = str(input("Digite seu nome completo: ")).strip()
+
+print("-" * 100)
+print(f"Seu primeiro nome é {nome_completo[0:nome_completo
+      .find(" ")].title()}."
+      f"\nSeu último sobrenome é {nome_completo[nome_completo
+      .rfind(" ") + 1:].title()}.")
