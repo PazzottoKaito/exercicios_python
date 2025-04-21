a8de64a3ed9e318ffc8a8839c6fca8271caf6002
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-21 14:55:38 -0300

    Adicionando o exercício 041

---
+++ exercicio_041.py
@@ -0,0 +1,26 @@
+#Programa para categorizar faixa etária de atletas
+import datetime
+
+ano = int(input("Digite seu ano de nascimento: "))
+hoje = datetime.date.today().year
+idade = hoje - ano
+categoria = ""
+
+print("-" * 100)
+
+if ano < (hoje - 100) or ano >= hoje:
+    print("Digite um ano válido.")
+else:
+    if idade <= 9:
+        categoria = "mirim"
+    elif idade <= 14:
+        categoria = "infantil"
+    elif idade <= 19:
+        categoria = "júnior"
+    elif idade <= 25:
+        categoria = "sênior"
+    else:
+        categoria = "master"
+
+    print(f"Com {idade} ano(s) de idade, sua classificação é: "
+          f"{categoria.capitalize()}.")
