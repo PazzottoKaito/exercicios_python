58a898d41f5d91834143e264f6bfd56bb176c2f1
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-21 15:06:18 -0300

    Adicionando o exercício 042

---
+++ exercicio_042.py
@@ -0,0 +1,22 @@
+#Programa para verificar e classificar um triângulo
+
+lado_a = int(input("Digite a primeira medida: "))
+lado_b = int(input("Digite a segunda medida: "))
+lado_c = int(input("Digite a terceira medida: "))
+tipo = ""
+
+print("-" * 100)
+
+if (lado_a + lado_b > lado_c and lado_c + lado_a > lado_b and
+        lado_b + lado_c > lado_a):
+    if lado_a == lado_b == lado_c:
+        tipo = "equilátero"
+    elif lado_a != lado_b and lado_b != lado_c and lado_c != lado_a:
+        tipo = "escaleno"
+    else:
+        tipo = "isósceles"
+
+    print(f"Com essas medidas, é possível montar um triângulo {tipo}.")
+
+else:
+    print(f"Com essas medidas, não é possível montar um triângulo.")
