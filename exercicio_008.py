02ec7dc0d396cdf24842147020641eb5cf85f8dc
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:42:45 -0300

    Atualizando o exercício 008

--- exercicio_008.py
+++ exercicio_008.py
@@ -3,11 +3,12 @@
 medida = float(input("Digite uma medida, em metros: "))
 
 print("-" * 100)
+
 print(f"A medida digitada é equivalente a: "
-      f"\n{medida * 1000:.0f} milímetros (mm)."
-      f"\n{medida * 100:.0f} centímetros (cm)."
-      f"\n{medida * 10:.0f} decímetros (dm)."
-      f"\n{medida:.0f} metros (m)."
+      f"\n{medida * 1000:.2f} milímetros (mm)."
+      f"\n{medida * 100:.2f} centímetros (cm)."
+      f"\n{medida * 10:.2f} decímetros (dm)."
+      f"\n{medida} metros (m)."
       f"\n{medida / 10} decâmetros (dam)."
       f"\n{medida / 100} hectômetros (hm)."
       f"\n{medida / 1000} quilômetros (km).")
