ab22f6947a248490619eba3941b36f6647cd3574
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:37:04 -0300

    Atualizando o exercício 006

--- exercicio_006.py
+++ exercicio_006.py
@@ -4,6 +4,7 @@ import math
 numero = int(input("Digite um número: "))
 
 print("-" * 100)
+
 print(f"Seu dobro equivale a {numero * 2}."
       f"\nSeu triplo equivale a {numero * 3}."
       f"\nSua raiz quadrada equivale a {math.sqrt(numero):.2f}.")
