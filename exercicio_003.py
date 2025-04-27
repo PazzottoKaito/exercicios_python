8289ed5d3d18e450c3022ff67d67863702ebf057
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:31:22 -0300

    Atualizando o exercício 003

--- exercicio_003.py
+++ exercicio_003.py
@@ -2,9 +2,8 @@
 
 numero_a = int(input("Digite o primeiro número: "))
 numero_b = int(input("Digite o segundo número: "))
-soma = numero_a + numero_b
 
 print("-" * 100)
 
 print(f"A soma entre os números {numero_a} e {numero_b} "
-      f"é equivalente a {soma}.")
+      f"é equivalente a {numero_a + numero_b}.")
