1b9e761da418d56038db3e1762e724ded68db01d
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:33:01 -0300

    Atualizando o exercício 004

--- exercicio_004.py
+++ exercicio_004.py
@@ -3,15 +3,18 @@
 texto = str(input("Digite algo: "))
 
 print("-" * 100)
+
 print(f"O tipo primitivo desse dado é: {type(texto)}.")
 print(f"O dado é um espaço vazio: {texto.isspace()}.")
 
 print("-" * 100)
+
 print(f"O dado é apenas alfabético: {texto.isalpha()}.")
 print(f"O dado é apenas numérico: {texto.isnumeric()}.")
 print(f"O dado é alfanumérico: {texto.isalnum()}.")
 
 print("-" * 100)
+
 print(f"O dado está em letras minúsculas: {texto.islower()}.")
 print(f"O dado está em letras maiúsculas: {texto.isupper()}.")
 print(f"O dado está capitalizado: {texto.istitle()}.")
