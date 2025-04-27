68066266d63b84dd5680f088e3a61f2a2aaaf754
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:47:08 -0300

    Atualizando o exercício 010

--- exercicio_010.py
+++ exercicio_010.py
@@ -3,9 +3,10 @@
 reais = float(input("Digite quantos reais você possui: R$"))
 
 print("-" * 100)
-print(f"Em 02/03/2025, esse valor equivale aproximadamente a: "
-      f"\n${reais / 5.88:.2f} - Dólares americanos (USD)."
-      f"\n£{reais / 7.41:.2f} - Libras esterlinas (GBP)."
-      f"\n€{reais / 6.13:.2f} - Euros (EUR)."
+
+print(f"Em 26/04/2025, esse valor equivale aproximadamente a: "
+      f"\n${reais / 5.69:.2f} - Dólares americanos (USD)."
+      f"\n£{reais / 7.57:.2f} - Libras esterlinas (GBP)."
+      f"\n€{reais / 6.46:.2f} - Euros (EUR)."
       f"\n¥{reais / 0.81:.2f} - Yuans chineses (CNY)."
       f"\n¥{reais / 0.04:.2f} - Yenes japoneses (JPY).")
