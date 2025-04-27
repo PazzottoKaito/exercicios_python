98d0ceba46ac846385f3d01ae49ddd39d20c0fee
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-04-26 21:39:11 -0300

    Atualizando o exercício 007

--- exercicio_007.py
+++ exercicio_007.py
@@ -2,8 +2,8 @@
 
 nota_a = float(input("Digite a primeira nota: "))
 nota_b = float(input("Digite a segunda nota: "))
-media = (nota_a + nota_b) / 2
 
 print("-" * 100)
+
 print(f"Com as notas {nota_a:.2f} e {nota_b:.2f}, a média final é "
-      f"equivalente a {media:.2f}.")
+      f"equivalente a {(nota_a + nota_b) / 2:.2f}.")
