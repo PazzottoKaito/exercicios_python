bbe380a1682ab8d3e890fa3bc4342398a9a86229
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-02 22:21:27 -0300

    Atualizando o exercício 016

--- exercicio_016.py
+++ exercicio_016.py
@@ -1,4 +1,7 @@
-#Programa para conversão de números decimais em naturais
+#Programa para conversão de números decimais em inteiros
 
 numero = float(input("Digite um número decimal: "))
+
+print("-" * 100)
+
 print(f"A porção inteira de {numero} é {int(numero)}.")
