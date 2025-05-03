2e6a36e56bebddfc581265c09bc5f7e36c0322e3
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-02 22:41:03 -0300

    Atualizando o exercício 023

--- exercicio_023.py
+++ exercicio_023.py
@@ -3,8 +3,9 @@
 numero = int(input("Digite um número com quatro algarismos: "))
 
 print("-" * 100)
+
 print(f"O número digitado, {numero}, é composto por:"
       f"\n{numero // 1000 % 10} unidade(s) de milhar."
       f"\n{numero // 100 % 10} centena(s)."
       f"\n{numero // 10 % 10} dezena(s)."
-      f"\n{numero // 1 % 10} unidade(s).")
+      f"\n{numero % 10} unidade(s).")
