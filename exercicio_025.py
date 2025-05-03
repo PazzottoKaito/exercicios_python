82b435c343d18458e157bc08d479423c7f32e4e2
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-02 22:47:56 -0300

    Atualizando o exercício 025

--- exercicio_025.py
+++ exercicio_025.py
@@ -1,8 +1,9 @@
 #Programa para procurar um sobrenome específico
 
-nome_completo = (str(input("Digite seu nome completo: ")).strip()
+nome_completo = (input("Digite seu nome completo: ").strip()
                  .lower())
 
 print("-" * 100)
-print(f"O seu nome completo possui o nome ou sobrenome 'Silva': "
+
+print(f"O seu nome completo contém 'Silva' em alguma parte: "
       f"{'silva' in nome_completo}.")
