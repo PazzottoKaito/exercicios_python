53d9d0439ea8c82852e8dc01a56495e1d158e0ec
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-02 22:24:54 -0300

    Atualizando o exercício 017

--- exercicio_017.py
+++ exercicio_017.py
@@ -1,11 +1,14 @@
 #Programa para calcular o valor da hipotenusa
 import math
 
-cateto_adjacente = float(input("Digite o valor do cateto adjacente: "))
-cateto_oposto = float(input("Digite o valor do cateto oposto: "))
+cateto_adjacente = float(input("Digite o valor do cateto adjacente "
+                               "em centímetros: "))
+cateto_oposto = float(input("Digite o valor do cateto oposto em "
+                            "centímetros: "))
 hipotenusa = math.hypot(cateto_adjacente,
                         cateto_oposto)
 
-print("-" * 100)
+print("-" * 150)
+
 print(f"A hipotenusa deste triângulo retângulo equivale a "
-      f"{hipotenusa:.2f}.")
+      f"{hipotenusa:.2f} centímetros.")
