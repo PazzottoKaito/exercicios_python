3848d178c263cb4613653e30b3b16698f2b9df42
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-02 22:28:29 -0300

    Atualizando o exercício 018

--- exercicio_018.py
+++ exercicio_018.py
@@ -5,6 +5,7 @@ angulo = int(input("Digite um ângulo: "))
 radiano = math.radians(angulo)
 
 print("-" * 100)
+
 print(f"O seno do ângulo de {angulo}° é {math.sin(radiano):.2f}."
       f"\nO cosseno do ângulo de {angulo}° é {math.cos(radiano):.2f}."
       f"\nA tangente do ângulo de {angulo}° é {math.tan(radiano):.2f}.")
