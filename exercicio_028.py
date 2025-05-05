e1c50c334aa6ac1c42a800593ab3166750e168dc
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-04 21:12:56 -0300

    Atualizando o exercício 028

--- exercicio_028.py
+++ exercicio_028.py
@@ -1,18 +1,18 @@
 #Programa para adivinhar o número sorteado
 import random
 
-numero = int(input("Digite um número de 0 a 5: "))
-sorteado = random.randint(0, 5)
+numero = int(input("Digite um número de 1 a 5: "))
+sorteado = random.randint(1, 5)
 
 print("-" * 100)
 
-if 5>= numero >= 0:
+if 1 <= numero <= 5:
     if numero == sorteado:
         print(f"Eu escolhi o número {sorteado} e você também."
-              f"\nParabéns!!")
+              f"\nParabéns, você adivinhou meu número!!")
     else:
         print(f"Eu escolhi o número {sorteado} e você escolheu "
               f"o número {numero}."
-              f"\nMais sorte na próxima vez...")
+              f"\nMais sorte na próxima vez adivinhando meu número...")
 else:
-    print("Você deve escolher um número entre 0 e 5.")
+    print("Escolha um número entre 1 e 5.")
