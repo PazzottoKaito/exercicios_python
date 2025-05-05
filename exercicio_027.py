6bd03f88c9cc41921551a7cac7eda1690bb22056
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-04 21:06:18 -0300

    Atualizando o exercício 027

--- exercicio_027.py
+++ exercicio_027.py
@@ -3,7 +3,8 @@
 nome_completo = str(input("Digite seu nome completo: ")).strip()
 
 print("-" * 100)
-print(f"Seu primeiro nome é {nome_completo[0:nome_completo
-      .find(" ")].title()}."
-      f"\nSeu último sobrenome é {nome_completo[nome_completo
-      .rfind(" ") + 1:].title()}.")
+
+print(f"Seu primeiro nome é {nome_completo[0:nome_completo.
+      find(' ')].title()}."
+      f"\nSeu último sobrenome é {nome_completo[nome_completo.
+      rfind(' ') + 1:].title()}.")
