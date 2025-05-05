52ba7ca4df52e01d966967caa9b0f6b636103099
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-04 21:34:14 -0300

    Atualizando o exercício 031

--- exercicio_031.py
+++ exercicio_031.py
@@ -2,15 +2,15 @@
 
 quilometros = int(input("Digite quantos quilômetros foram "
                         "percorridos na viagem: "))
-taxa = 0
 
 print("-" * 100)
 
-if 0 < quilometros <= 200:
-    taxa = 0.50
-elif quilometros > 200:
-    taxa = 0.45
+if quilometros > 0:
+    if quilometros <= 200:
+        print(f"O total a ser pago é R${quilometros * 0.50:.2f}."
+              f"\nCada quilômetro custou R$0,50.")
+    else:
+        print(f"O total a ser pago é R${quilometros * 0.45:.2f}."
+              f"\nCada quilômetro custou R$0,45.")
 else:
     print("Digite uma quilometragem válida.")
-
-print(f"O total a ser pago por essa viagem é R${quilometros * taxa:.2f}.")
