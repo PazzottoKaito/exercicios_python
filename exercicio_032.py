bd152914dfb42b15ec3362c450561d54fc4ecf35
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-04 22:02:00 -0300

    Atualizando o exercício 032

--- exercicio_032.py
+++ exercicio_032.py
@@ -1,11 +1,13 @@
 #Programa para saber se um ano é bissexto ou não
 
 ano = int(input("Digite um ano: "))
-resposta = ""
 
-if ano % 4 == 0:
-    resposta = "é um ano bissexto"
+print("-" * 100)
+
+if ano > 0:
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        print(f"O ano de {ano} é bissexto!!")
+    else:
+        print(f"O ano de {ano} não é bissexto!!")
 else:
-    resposta = "não é um ano bissexto"
-
-print(f"O ano de {ano} {resposta}!!")
+    print("Digite um ano válido.")
