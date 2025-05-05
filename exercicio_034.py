28eacb943e8d37be5f8a394ac1b781cd1bc2bd83
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-05-04 22:11:55 -0300

    Atualizando o exercício 034

--- exercicio_034.py
+++ exercicio_034.py
@@ -1,17 +1,18 @@
 #Programa para aumento de salário com condições
 
 salario = float(input("Digite o valor do salário: R$"))
-aumento = 0
 
-print("-" * 100)
+print("-" * 150)
 
 if 0 < salario <= 1250:
-    aumento = 0.15 * salario
-    print(f"Com 15% de aumento, o novo valor do salário é de "
-          f"R${salario + aumento:.2f}.")
+    aumento = salario * 0.15
+    novo_salario = salario + aumento
+    print(f"Com 15% de aumento, o salário aumentou R${aumento:.2f} "
+          f"e passou a ser R${novo_salario:.2f}.")
 elif salario > 1250:
-    aumento = 0.10 * salario
-    print(f"Com 10% de aumento, o novo valor do salário é de "
-          f"R${salario + aumento:.2f}.")
+    aumento = salario * 0.10
+    novo_salario = salario + aumento
+    print(f"Com 10% de aumento, o salário aumentou R${aumento:.2f} "
+          f"e passou a ser R${novo_salario:.2f}.")
 else:
     print("Digite um valor de salário válido.")
