180285d14e006e22b352d7bf102dfe8b60e060a2
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-06-29 22:37:41 -0300

    Atualizando o exercício 037

--- exercicio_037.py
+++ exercicio_037.py
@@ -3,19 +3,22 @@
 numero = int(input("Digite um número inteiro: "))
 
 print("-" * 100)
-print("Digite [1] para converter em binário."
-      "\nDigite [2] para converter em octal."
-      "\nDigite [3] para converter em hexadecimal.")
+
+print("Digite [1] para conversão em binário."
+      "\nDigite [2] para conversão em octal."
+      "\nDigite [3] para conversão em hexadecimal.")
+
 print("-" * 100)
 
 opcao = int(input("Digite uma opção: "))
+
 print("-" * 100)
 
 if opcao == 1:
-    print(f"O número {numero} em base binária é: {bin(numero)}.")
+    print(f"O número digitado em base binária é: {bin(numero)}.")
 elif opcao == 2:
-    print(f"O número {numero} em base octal é: {oct(numero)}.")
+    print(f"O número digitado em base octal é: {oct(numero)}.")
 elif opcao == 3:
-    print(f"O número {numero} em base hexadecimal é: {hex(numero)}.")
+    print(f"O número digitado em base hexadecimal é: {hex(numero)}.")
 else:
     print("Opção inválida.")
