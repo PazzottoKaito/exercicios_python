96e1c4c15e7ba754827edd97e241dcd73115eeef
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-06-29 22:24:05 -0300

    Atualizando o exercício 036

--- exercicio_036.py
+++ exercicio_036.py
@@ -2,15 +2,18 @@
 
 valor = float(input("Digite o valor do imóvel: R$"))
 salario = float(input("Digite o salário mensal do comprador: R$"))
-anos = int(input("Digite em quantos anos se pretende quitar o "
+anos = int(input("Digite em quantos anos se pretende quitar esse "
                  "financiamento: "))
 
 print("-" * 150)
 
-print(f"Para quitar o financiamento em {anos} anos, é necessária "
-          f"uma parcela mensal de R${valor / (anos * 12):.2f}.")
+if valor > 0 and salario > 0 and anos > 0:
+    print(f"Para quitar o financiamento em {anos} anos, é necessária "
+              f"uma parcela mensal de R${valor / (anos * 12):.2f}.")
 
-if valor / (anos * 12) > salario * 0.30:
-    print("Diante disso, o financiamento foi negado!!")
+    if valor / (anos * 12) > salario * 0.30:
+        print("Diante disso, o financiamento foi negado!!")
+    else:
+        print("Diante disso, o financiamento foi aprovado e concedido!!")
 else:
-    print("Diante disso, o financiamento foi aprovado e concedido!!")
+    print(f"Digite valores válidos.")
