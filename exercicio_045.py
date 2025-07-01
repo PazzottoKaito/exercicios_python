c18e488c70181cb4a7b9c2c44242ade82b4f7c43
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-06-30 22:58:32 -0300

    Atualizando o exercício 045

--- exercicio_045.py
+++ exercicio_045.py
@@ -1,16 +1,20 @@
 #Programa para jogar pedra, papel e tesoura
 import random
 
+print("-" * 100)
+
 print("Digite [1] para jogar pedra."
       "\nDigite [2] para jogar papel."
       "\nDigite [3] para jogar tesoura.")
 
 print("-" * 100)
+
 opcao_jogador = int(input("Digite qual item você quer jogar: "))
 itens = ("pedra", "papel", "tesoura")
 opcao_maquina = random.randint(1, 3)
 
 print("-" * 100)
+
 if opcao_jogador < 1 or opcao_jogador > 3:
     print("Digite uma opção válida.")
 else:
@@ -22,6 +26,6 @@ else:
         resposta = "Você venceu!!"
     else:
         resposta = "Eu venci!!"
-
-    print(f"Você jogou {itens[opcao_jogador - 1]}... Eu joguei "
-          f"{itens[opcao_maquina - 1]}... {resposta}")
+    print(f"Você jogou {itens[opcao_jogador - 1]}... "
+          f"\nEu joguei {itens[opcao_maquina - 1]}... "
+          f"\n{resposta}")
