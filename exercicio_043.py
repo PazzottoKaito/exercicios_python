333aff01ce5ee6eb4920f8cdfd248bc82ec20f1c
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-06-30 22:50:18 -0300

    Atualizando o exercício 043

--- exercicio_043.py
+++ exercicio_043.py
@@ -1,4 +1,4 @@
-#Programa para cálculo de índice de massa corporal
+#Programa para cálculo de IMC
 
 altura = int(input("Digite sua altura, em centímetros: ")) / 100
 peso = int(input("Digite seu peso, em quilogramas: "))
@@ -9,7 +9,6 @@ if altura <= 0 or peso <= 0:
     print("Digite valores válidos.")
 else:
     formula = peso / (altura ** 2)
-
     if formula <= 18.5:
         avaliacao = "abaixo do peso"
     elif formula < 25:
@@ -20,6 +19,5 @@ else:
         avaliacao = "obesidade"
     else:
         avaliacao = "obesidade mórbida"
-
     print(f"Seu IMC é de {formula:.2f}, com avaliação de: "
           f"{avaliacao.capitalize()}.")
