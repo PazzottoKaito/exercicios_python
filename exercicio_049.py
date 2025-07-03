e7c5674f3e930aea108b946d2ba10d4b29a44d16
Fellipe Pazzotto <fellipepazzotto@outlook.com>
2025-07-02 22:40:55 -0300

    Atualizando o exercício 049

--- exercicio_049.py
+++ exercicio_049.py
@@ -1,9 +1,12 @@
-#Programa de tabuada de um número escolhido pelo usuário
+#Programa de tabuada de um número específico
 
 numero = int(input("Digite um número inteiro para ser tabulado: "))
 
 print("-" * 100)
-print(f"A tabuada de multiplicação de 1 a 10 do número {numero}:")
+
+print(f"A tabuada de multiplicação do número {numero}")
+
+print("-" * 100)
 
 for numeros in range(1, 11):
     print(f"{numero} X {numeros:02d} = {numero * numeros}")
